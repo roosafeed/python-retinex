6a15dabb40dfa42d5248a336f193d6b2ec85dc98
roosafeed <roosafeedkoyakp811@gmail.com>
2021-04-07 20:46:04 +0530

    added README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Multiscale retinex for color restoration
+
+## Description
+Multiscale retinex filter for image color restoration implemented in Python.
+
+## Requirement
+* Python 3
+* Numpy
+* OpenCV
+
+## How to
+Create a folder named 'images' in the same directory as the python codes. Place all the input files inside the 'images' folder.
+
+Parameters of the retinex filter can be changed in 'run.py', the variables under the comment 'contants'
+
+## Reference
+* [Multiscale Retinex] (http://www.ipol.im/pub/art/2014/107/)
--- msr.py
+++ msr.py
@@ -5,31 +5,23 @@ import cv2
 import os
 from gauss import gauss
 
-# constants
-sig_list = [15, 80, 250]    #list of sigma
-w = 1/3                     #weight
-k = (0, 0)                  #kernel size
-a = 125                     #alpha
-G = 192
-b = -30
-bet = 46                    #beta
 cv2_gaussblur = False       #whether or not to use cv2.GaussianBlur
 
-def SSR(img, sigma):
+def SSR(img, sigma, kernel):
     if(cv2_gaussblur):
-        return np.log10(img) - np.log10(cv2.GaussianBlur(img, k, sigma))
+        return np.log10(img) - np.log10(cv2.GaussianBlur(img, kernel, sigma))
 
     out = np.zeros(img.shape)
     for i in range(img.shape[2]):
-        out[:, :, i] = gauss(img[:, :, i], k, sigma)
+        out[:, :, i] = gauss(img[:, :, i], kernel, sigma)
     out = out + 1   #prevent division by zero
     return np.log10(img) - np.log10(out)
     
-def MSR(img, sigmas, weight):
+def MSR(img, sigmas, weight, kernel):
     retinex = np.zeros(img.shape)
 
     for s in sigmas:
-        retinex += SSR(img, s)
+        retinex += SSR(img, s, kernel)
 
     retinex = weight * retinex
     return retinex
@@ -43,12 +35,12 @@ def CRF(img, alpha, beta):
     return cr
 
 
-def MSRCR(img, sigma_list, G, b, alpha, beta, low_clip, high_clip):
+def MSRCR(img, sigma_list, G, b, alpha, beta, weight=0.3, kernel_size=(0,0)):
     # Color restoration
     ep = 1   #to prevent division by zero
     img = np.float64(img) + ep
 
-    retinex = MSR(img, sigma_list, w)    
+    retinex = MSR(img, sigma_list, weight, kernel_size)    
     cl_res_fn = CRF(img, alpha, beta)    
     msrcr = G * (retinex * cl_res_fn - b)
 
@@ -58,26 +50,3 @@ def MSRCR(img, sigma_list, G, b, alpha, beta, low_clip, high_clip):
     msrcr = np.uint8(msrcr)  
 
     return msrcr
-
-# 
-file = '1.jpg'
-img = cv2.imread('img/' + file)
-
-out = MSRCR(img, sig_list, G, b, a, bet, 0.01, 0.99)
-
-path = 'output/'
-if not os.path.exists(path):
-    os.makedirs(path)
-path = path + 'MSRCR_'
-if(cv2_gaussblur):
-    path = path + 'cv2_'
-iw = cv2.imwrite(path + file.split('.')[0] + '.png', out)
-if(iw):
-    print('image saved to ' + path)
-
-
-cv2.imshow('Retinex Colour Restored', cv2.resize(out, (500,500)))
-cv2.imshow('Original', cv2.resize(img, (500,500)))
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
